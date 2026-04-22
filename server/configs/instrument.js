c4cece46d9f6d56614c4fd701a0333b555113aec
Naman Tyagi <namantyagi98665@gmail.com>
2026-04-22 20:29:25 +0530

    fix: remove secrets and add security safeguards

--- server/configs/instrument.js
+++ server/configs/instrument.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import * as Sentry from "@sentry/node";
 
 Sentry.init({
-  dsn: "https://4d9b8927308822cf23b66027e4b650f4@o4511224756961280.ingest.us.sentry.io/4511224784814080",
+  dsn: process.env.SENTRY_DSN,
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
